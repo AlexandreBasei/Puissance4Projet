7c11f163a3a6483726687ab2e173d54b76340624
tvantard <tvantard@univ-lyon2.fr>
2025-03-20 15:31:23 +0100

    Starting :D

---
+++ Puissance4Projet/Pui4ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Pui4ViewController.swift
+//  Puissance4Projet
+//
+//  Created by Timothe Vantard on 20/03/2025.
+//
+
+import UIKit
+
+class Pui4ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
