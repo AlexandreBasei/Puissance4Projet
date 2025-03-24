989d25661f8933fd4cfbf9e37dfacdc92810bc02
tvantard <tvantard@univ-lyon2.fr>
2025-03-24 11:08:36 +0100

    Commentaries

--- Puissance4Projet/Pui4ViewController.swift
+++ Puissance4Projet/Pui4ViewController.swift
@@ -18,13 +18,30 @@ class Pui4ViewController: UIViewController {
               [0,0,0,0,0,0,0],
               [0,0,0,0,0,0,0],]
     
-    var lastCoin : [Int]=[0,0]
+    var lastCoin : (Int,Int) = (-1,-1)
     
-    var directions : [(Int,Int)] = [(0, -1), (-1, -1), (-1,0), (1,-1)]
+    var directions : [(Int,Int)] = [(0, -1), (-1, -1), (-1,0), (1,-1)]      // dans l'ordre gauche, diagonale gh, haut, diagonale droite-haut
+    
+    
+    func mettreJeton(x: Int)->(Bool,(Int,Int)) {
+        for i in (0..<Grille.count).reversed() {
+            // on regarde à quelle ligne, la collone chosie est libre (donc on part du bas vers le haut car c'est plus simple dans notre esprit)
+            
+            if Grille[i][x]==0{
+                Grille[i][x]=currPlayer
+                lastCoin=(i,x)
+                return (true, lastCoin)
+            }
+        }
+        return (false, (-1,-1))
+        // si y'a pas de place, ça renvoie false
+    }
     
     func victoryCheck() -> Bool {
         for (dx,dy) in directions {
             let total = 1 + checkDirections(dir:(dx, dy)) + checkDirections(dir:(-dx, -dy))
+            // on regarde les axes horizontal, diagonal hg->bd, vertical et diagonal hd->bg
+            
             if total >= 4 {
                 return true
             }
@@ -34,9 +51,10 @@ class Pui4ViewController: UIViewController {
     
     func checkDirections(dir:(Int,Int))-> Int {
         var count=0
-        var coinX = lastCoin[0]; var coinY = lastCoin[1]
+        var coinX = lastCoin.0; var coinY = lastCoin.1
         
-        while 0 <= coinX && coinX < Grille[0].count && 0 <= coinY && coinY < Grille.count && Grille[coinY][coinX]==currPlayer{
+        while 0 <= coinX && coinX < Grille[0].count && 0 <= coinY && coinY < Grille.count && Grille[coinY][coinX]==currPlayer {
+            // parcours de l'axe jusqu'à une couleur différente.
             count += 1
             coinX += dir.0
             coinY += dir.1
