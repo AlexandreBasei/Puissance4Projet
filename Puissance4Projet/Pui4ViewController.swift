22c2c305e8e56ed141fe109752bb43fc75f398c9
tvantard <tvantard@univ-lyon2.fr>
2025-03-24 09:52:16 +0100

    TEST

--- Puissance4Projet/Pui4ViewController.swift
+++ Puissance4Projet/Pui4ViewController.swift
@@ -8,11 +8,51 @@
 import UIKit
 
 class Pui4ViewController: UIViewController {
-
+    
+    var currPlayer = 1
+    var Grille : [[Int]] =
+            [ [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],]
+    
+    var lastCoin : [Int]=[0,0]
+    
+    func victoryCheck() {
+        
+        
+    }
+    
+    func check(coin:[Int])-> Int {
+        var cpt = 1
+        let x=coin[0];let y=coin[1]
+        if x != 0 {
+            if y != 0 {
+                
+            }
+            if y != Grille.count{
+                
+            }
+        }
+        if x != Grille[0].count {
+            if y != 0 {
+                
+            }
+            if y != Grille.count{
+                
+            }
+        }
+        return 0
+    }
+            
+            
+            
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
-        // Do any additional setup after loading the view.
     }
     
 
